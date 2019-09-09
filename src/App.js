98edd0803a0f9aa605c25c2d4ee8bad5607e1106
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-09-09 21:37:54 +0100

    changed the value of touchdown to 7

--- src/App.js
+++ src/App.js
@@ -60,11 +60,11 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => { HomeScoreIncrement(6) }}>Home Touchdown</button>
+          <button className="homeButtons__touchdown" onClick={() => { HomeScoreIncrement(7) }}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick={() => { HomeScoreIncrement(3) }}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => AwayScoreIncrement(6)}>Away Touchdown</button>
+          <button className="awayButtons__touchdown" onClick={() => AwayScoreIncrement(7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick={() => AwayScoreIncrement(3)}>Away Field Goal</button>
         </div>
       </section>
